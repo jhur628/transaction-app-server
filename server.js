3b20198546c78f9c50eb75de58ec4b72a1a49f87
John Hur <hur.john628@gmail.com>
2022-05-23 02:28:36 -0500

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers')
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
