6f20142e494a1f5cb0b052415ea2e7cb00390d73
John Hur <hur.john628@gmail.com>
2022-05-22 20:51:59 -0500

    ssl postgres

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL){
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host : process.env.DB_HOST,
