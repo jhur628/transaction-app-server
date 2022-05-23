dd69799e187e8501a0159c731088d993845aee69
John Hur <hur.john628@gmail.com>
2022-05-23 12:51:06 -0500

    trying without plain:true

--- controllers/transactionRoutes.js
+++ controllers/transactionRoutes.js
@@ -4,8 +4,8 @@ const { Transaction } = require('../models');
 router.get('/', async (req, res) => {
   try {
     const transactionData = await Transaction.findAll();
-    const transactions = transactionData.map(transaction => transaction.get({ plain: true }));
-    res.status(200).json(transactions);
+    // const transactions = transactionData.map(transaction => transaction.get({ plain: true }));
+    res.status(200).json(transactionData);
   } catch (err) {
     res.status(400).json(err)
   }
