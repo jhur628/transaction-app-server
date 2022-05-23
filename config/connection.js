a15862edd5797f8d19946f4b221dba3c51264b9f
John Hur <hur.john628@gmail.com>
2022-05-22 19:34:36 -0500

    updated server.js and connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host : process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false
-})
+if (process.env.DATABASE_URL){
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host : process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false
+  });
+};
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers')
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
