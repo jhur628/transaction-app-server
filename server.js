a400f39fd6a68f0462439e14536215c137dc7f12
John Hur <hur.john628@gmail.com>
2022-05-22 23:10:50 -0500

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/connection');
 const routes = require('./controllers')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
